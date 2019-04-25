b8fa31ec2458751be7332f1ce553a4b696cea1b9
Jeremy Teela <2ndteela@gmail.com>
2019-04-25 09:09:08 -0600

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase'
+
+var config = {
+    apiKey: "AIzaSyCZ58uvnOGphyz7VdZTP5bChBNgov9YDsA",
+    authDomain: "i-miss-you-154a9.firebaseapp.com",
+    databaseURL: "https://i-miss-you-154a9.firebaseio.com",
+    projectId: "i-miss-you-154a9",
+    storageBucket: "i-miss-you-154a9.appspot.com",
+    messagingSenderId: "821315662587"
+  };
+
+export default firebase.initializeApp(config);
