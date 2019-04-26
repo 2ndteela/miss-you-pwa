a30f5ad926728b7735d36388bebb238bbe965e60
Jeremy Teela <2ndteela@gmail.com>
2019-04-25 19:26:27 -0600

    doing new timer dialog

--- src/comps/Login/index.js
+++ src/comps/Login/index.js
@@ -1,14 +1,65 @@
 import React, { Component } from 'react';
+import firebase from '../../firebase'
 import './style.css'
 
 class Login extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = { 
+            username: '',
+            password: ''
+         }
     }
 
-    checkLogin() {
-        this.props.history.push('/main')
+    login() {
+        if(!this.state.email) alert("Your email can't be blank")
+        else if (!this.state.password) alert("Your password can't be blank")
+        firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
+        .then(user => {
+            this.props.history.push('/main')
+
+        })
+        .catch(err => {
+            alert(err.message)
+        }) 
+    }
+
+    signUp() {
+        if(!this.state.username) alert("Your username can't be blank")
+        else if(!this.state.password) alert("Your password can't be blank")
+        else if(!this.state.email) alert("Your email can't be blank")
+
+        firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password)
+        .then(() => {
+            firebase.auth().currentUser.updateProfile({displayName: this.state.username})
+            .then(() => {
+                const newUser = firebase.auth().currentUser
+    
+                const toAdd = {
+                    username: newUser.displayName,
+                    connections: []
+                }
+    
+                firebase.database().ref('/users').push(toAdd)
+                this.props.history.push('/main')
+            })
+            .catch(err => {
+                alert(err.message)
+            })
+        })
+        .catch(err => alert(err.message))
+    }
+
+    update(feild, value) {
+        this.setState({
+            [feild]: value
+        })
+    }
+
+    componentDidMount() {
+        firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL)
+        .then(function() {});
+        if(firebase.auth().currentUser) this.history.push('/main')
     }
 
     render() { 
@@ -19,16 +70,20 @@ class Login extends Component {
                 </div>
                 <div id="login-container">
                     <div className="styled-input" style={{marginBottom: "16px"}}>
-                        <input />
+                        <input value={this.state.email} onChange={(e) => this.update('email', e.target.value)} />
                         <span>Email</span>
                     </div>
-                    <div className="styled-input">
-                        <input hidden />
+                    <div className="styled-input" style={{marginBottom: "16px"}}>
+                        <input value={this.state.password} type="password" onChange={(e) => this.update('password', e.target.value)} />
                         <span>Password</span>
                     </div>
+                    <div className="styled-input">
+                        <input value={this.state.username} onChange={(e) => this.update('username', e.target.value)} />
+                        <span>Username</span>
+                    </div>
                     <div id="login-buttons" >
-                        <button className="button-padding" onClick={() => this.checkLogin()}>Login</button>
-                        <button onClick={() => this.checkLogin()}>Sign Up</button>
+                        <button className="button-padding" onClick={() => this.login()}>Login</button>
+                        <button onClick={() => this.signUp()}>Sign Up</button>
                 </div>
                 </div>
             </div>
--- src/comps/Main/index.js
+++ src/comps/Main/index.js
@@ -2,19 +2,40 @@ import React, { Component } from 'react';
 import './style.css'
 import firebase from '../../firebase'
 import TimerBox from '../TimerBox/Timerbox'
+import { init } from 'events';
 
 class Main extends Component {
     constructor(props) {
         super(props);
         this.state = { 
-            connections: []
+            connections: [],
+            newTimer: {
+                year: 0,
+                month: 0,
+                day: 0,
+                hour: 0,
+                min: 0
+            }
          }
     }
 
     componentDidMount() {
+
+        //const user = firebase.auth().currentUser
+        //if(!user) this.props.history.push('/')
+
+        const NOW = new Date() 
+
+        this.setState({
+            year: parseInt(NOW.getFullYear(), 10),
+            month: parseInt(NOW.getMonth(), 10) + 1,
+            day: parseInt(NOW.getDate(), 10),
+            hour: parseInt(NOW.getHours(), 10) + 1,
+            mins: 0
+        })
+
         firebase.database().ref('/users/-LZBM37T1gd7DuLKgxQm').once('value')
         .then(snapshot => {
-            console.log(snapshot.val())
 
             const data = snapshot.val()
             if(!!data.connections) {
@@ -26,9 +47,16 @@ class Main extends Component {
                 this.setState({
                     connections: arr
                 })
+
+                localStorage['connections'] = JSON.stringify(arr)
             }
             
-        }) 
+        })
+        .catch(err => {
+            this.setState({
+                connections: JSON.parse(localStorage['connections'])
+            })
+        })
     }
 
     render() { 
@@ -43,6 +71,47 @@ class Main extends Component {
                             return <TimerBox data={connection} key={itr + '-con'}  />
                         })
                     }
+                </div>
+                <button id="action-button">+</button>
+                <div id="dialog-container">
+                    <div id="dialog-background"></div>
+                    <div id="dialog-box">
+                        <h1>New Timer</h1>
+                        <div className="styled-input button-input">
+                            <button>+</button>
+                            <input type="number" value={this.state.year} />
+                            <button>-</button>
+                            <span>Year</span>
+                        </div>
+                        <div className="styled-input button-input">
+                            <button>+</button>
+                            <input type="number" value={this.state.month} />
+                            <button>-</button>
+                            <span>Month</span>
+                        </div>
+                        <div className="styled-input button-input">
+                            <button>+</button>
+                            <input type="number" value={this.state.day} />
+                            <button>-</button>
+                            <span>Date</span>
+                        </div>
+                        <div className="styled-input button-input">
+                            <button>+</button>
+                            <input type="number" value={this.state.hour} />
+                            <button>-</button>
+                            <span>Hour</span>
+                        </div>
+                        <div className="styled-input button-input">
+                            <button>+</button>
+                            <input type="number" value={this.state.mins} />
+                            <button>-</button>
+                            <span>Minutes</span>
+                        </div>
+                        <div className="button-box" >
+                            <button className="button-padding">Save</button>
+                            <button>Cancel</button>
+                        </div>
+                    </div>
                 </div>
             </div>
          );
